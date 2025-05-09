270bd6c9dfca8a947775d60e582880848986c7d3
Radha <radhakrishnan.varatharajan@twilightitsolutions.com>
2025-05-09 18:33:01 +0530

    Added and Refactored code for Multiple order Booking

--- tests/MultipleOrderBooking.spec.ts
+++ tests/MultipleOrderBooking.spec.ts
@@ -3,26 +3,26 @@ import { AdminHelper } from '../utils/adminHelper';
 import { testData } from '../fixture/testData';
 import { launchBrowserAt, createAdminContext, createKotContext, createUserContext } from '../utils/browserHelper';
 
-const menu ='Chicken Biriyani';
+const menu = 'Chicken Biriyani';
+
 // Test Suite
 test.describe('@E2E', () => {
-  test('User submits request and admin approves it', async () => {
+  test('User submits a request and admin approves it', async () => {
     test.setTimeout(120_000);
-    // 1. Launch browser in headed mode with window size
-
-    // 2. Create browser contexts for each role
+    
+    // 1. Launch the browser in headed mode with a specified window size
+    
+    // 2. Create browser contexts for different roles
     const adminContext = await createAdminContext();
     const kotContext = await createKotContext();
     const serverContext = await createKotContext();
     const userContext = await createUserContext();
 
-
     // 3. Admin logs in
     const adminPage = await adminContext.newPage();
     await AdminHelper.login(adminPage, testData.ADMIN_URL, testData.APPROVE_LIST_URL, testData.ADMIN_CREDENTIALS, 'Admin');
 
-
-    // 4. Fetch subscription value from admin panel
+    // 4. Fetch the subscription value from the admin panel
     const subscriptionValue = await AdminHelper.getAdminSubscription(adminPage);
 
     // 5. User submits a request using mobile simulation
@@ -37,7 +37,7 @@ test.describe('@E2E', () => {
     await AdminHelper.checkUserApproved(adminPage, testData.APPROVE_LIST_URL, subscriptionValue);
 
     // 8. User places an order
-    await AdminHelper.searchAndPlaceOrder(userPage, menu, 1);
+    await AdminHelper.searchAndPlaceForMultipleOrder(userPage, menu, 3);
     await AdminHelper.checkOrderStatus(userPage, menu, 'Placed');
 
     // 9. KOT logs in to manage orders
@@ -45,33 +45,21 @@ test.describe('@E2E', () => {
     await AdminHelper.login(kotPage, testData.KOT_URL, testData.ORDER_LIST_URL, testData.KOT_CREDENTIALS, 'KOT');
 
     // 10. KOT prepares and dispatches the order
-    await AdminHelper.prepareAndDispatchOrder(kotPage, menu, 'Started');
+    await AdminHelper.prepareAndDispatchForMultipleOrder(kotPage, menu, 'Starter 3');
     // await AdminHelper.prepareAndDispatchOrder(kotPage, menu, 'Dispatched');
 
-    // 9. KOT logs in to manage orders
-    const kotPage1 = await kotContext.newPage();
-    await AdminHelper.login(kotPage1, testData.KOT_URL, testData.ORDER_LIST_URL, testData.KOT_CREDENTIALS1, 'KOT');
-
-    // 9. KOT logs in to manage orders
-    const kotPage2 = await kotContext.newPage();
-    await AdminHelper.login(kotPage2, testData.KOT_URL, testData.ORDER_LIST_URL, testData.KOT_CREDENTIALS2, 'KOT');
-
-
-    await AdminHelper.login(kotPage, testData.KOT_URL, testData.ORDER_LIST_URL, testData.KOT_CREDENTIALS, 'KOT');
-    await AdminHelper.prepareAndDispatchOrder(kotPage, menu, 'Dispatched');
-
-    // 11. User verifies that the order status is updated
+    // 11. User checks if the order status has been updated
     await AdminHelper.checkOrderStatus(userPage, menu, 'Dispatched');
 
-    // 12. Server logs in and completes order
+    // 12. Server logs in and completes the order
     const serverPage = await serverContext.newPage();
     await AdminHelper.login(serverPage, testData.SERVER_URL, testData.SERVER_LIST_URL, testData.SERVER_CREDENTIALS, 'Server');
-    await AdminHelper.moveOrderStatus(serverPage, menu, 'Done');
+    await AdminHelper.moveMultipleOrderStatus(serverPage);
 
-    // 13. Final user check: order delivered
+    // 13. Final user check: confirm order is delivered
     await AdminHelper.checkOrderStatus(userPage, menu, 'Delivered');
 
-    //14. user switch to order list page
+    // 14. User switches to the order list page
     await AdminHelper.switchToOrderListPage(userPage);
     await AdminHelper.RequestedForBill(adminPage, testData.APPROVE_LIST_URL, subscriptionValue);
     await AdminHelper.closeBill(adminPage, testData.APPROVE_LIST_URL, subscriptionValue);
